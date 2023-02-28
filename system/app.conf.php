f769af7dc12954a2c59135afddfe3ec66058947b
strophy <badfunkstripe@gmail.com>
2023-02-28 15:29:29 +1100

    chore: bump version to 0.9.2

--- system/app.conf.php
+++ system/app.conf.php
@@ -1,6 +1,6 @@
 <?php
 define('APPLICATION_NAME', 'MultiFaucet');
 define('APPLICATION_WEBSITE', 'http://www.unibia.net/crypto-faucet');
-define('APPLICATION_VERSION', '0.9.1');
+define('APPLICATION_VERSION', '0.9.2');
 define('APPLICATION_CONFDIR', (@$MULTISITE_CONFDIR ?: 'config/')); //Must have trailing '/'
 ?>
